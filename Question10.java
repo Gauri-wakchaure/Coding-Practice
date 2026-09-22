c353d6193aa102834ab413ae19ba7caaa8445d1d
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-22 16:46:40 +0530

    Solve Question10

---
+++ Question10.java
@@ -0,0 +1,13 @@
+class Question10 {
+    public static void main(String[] args) {
+        int Array[] = {12,34, 9, 87, 32, 90, 56, 28,78};
+        int largest_ele = Array[0];
+
+        for(int i = 0; i<Array.length; i++) {
+            if(Array[i] > largest_ele) {
+                largest_ele = Array[i];
+            }
+        }
+        System.out.println(largest_ele+ " is the largest Element from Array");
+    }
+}
