81664a49997562229e316562ea7eece173e8f8b2
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-22 13:40:30 +0530

    Solve Question1

---
+++ Question1.java
@@ -0,0 +1,12 @@
+class Question1 {
+    public static void main(String[] args) {
+          int num = 98;
+        if(num>0) {
+            System.out.println(num+" is Positive number");
+        }else if(num<0){
+            System.out.println(num+" is Negative number");
+        }else if(num==0) {
+            System.out.println(num+" is Zero");
+        }
+    }
+}
