ddbffe8951272286c7ebd451042352b6c5a89596
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-22 14:00:49 +0530

    Solve Question3

---
+++ Question3.java
@@ -0,0 +1,19 @@
+class Question3 {
+    public static void main(String[] args) {
+          int num = 15;
+          int count = 0;
+    
+        for(int i=1; i<50; i++) {
+            if(num % i == 0) {
+                count++;
+            }
+        }
+        
+         if(count==2) {
+             System.out.println("Prime number");
+         }else{
+             System.out.println("Not a Prime number");
+         }
+        
+    }
+}
