048dc4beafebcb231b4ea78d2df77c56759fe1a0
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-22 16:39:18 +0530

    Solve Question9

---
+++ Question9.java
@@ -0,0 +1,23 @@
+class Question9 {
+    public static void main(String[] args) {
+        Student s1 = new Student("Gauri", 92);
+        s1.displayInfo();
+
+        Student s2 = new Student("Rehaa", 87);
+        s2.displayInfo();
+    }
+}
+class Student {
+    String name;
+    int marks;
+
+    public void displayInfo() {
+        System.out.println(this.name);
+        System.out.println(this.marks);
+    }
+
+    Student(String name, int marks) {
+        this.name = name;
+        this.marks = marks;
+    }
+}
