9fb96ca3a3e0d14337476d8662b975135ce12076
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-22 13:47:33 +0530

    Solve Question2

---
+++ Question2.java
@@ -0,0 +1,15 @@
+class Question2 {
+    public static void main(String[] args) {
+          int num1 = 9811;
+          int num2 = 993;
+          int num3 = 3477;
+        if(num1>num2 && num1>num3) {
+            System.out.println("First Number is Greater");
+        }else if(num2>num3) {
+            System.out.println("Second Number is Greater");
+        }else{
+            System.out.println("Third Number is Greater");
+        }
+        
+    }
+}
