50822c739c31d28b80c82342374b9c6eccdc3781
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-22 14:57:06 +0530

    Solve Question6

---
+++ Question6.java
@@ -0,0 +1,19 @@
+class Question6 {
+    public static void main(String[] args) {
+          int num = 121;
+          int copy = num;
+          int result = 0;
+
+         while(num != 0) {
+         int last_digit = num % 10;
+         num = num / 10;
+         result = result * 10 + last_digit;
+         }
+
+        if(copy == result) {
+            System.out.println(result+" is Palindrome number");
+        }else {
+            System.out.println(result+" is not Palidrome number");
+        }
+    }
+}
