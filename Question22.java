e68152dbbf003ac94037c3a08dcdfd27378fb104
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-23 16:35:40 +0530

    Solve Question22

---
+++ Question22.java
@@ -0,0 +1,13 @@
+public class Question22 {
+    public static void main(String[] args) {
+
+        int num1 = 22;
+        int num2 = 0;
+
+        try {
+            int result = num1 / num2;
+        } catch(Exception e) {
+            System.out.println("Any number Can't divide by Zero");
+        }       
+    }
+}
