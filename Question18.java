a767f32447d5a888645caef95bce9c27fbfeb48e
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-23 14:12:08 +0530

    Solve Question18

---
+++ Question18.java
@@ -0,0 +1,17 @@
+public class Question18 {
+    public static void main(String[] args) {
+        Find_Largest f1 = new Find_Largest();
+        System.out.println(f1.findLargest(23,56,45));   
+    }
+}
+class Find_Largest {
+        public static int findLargest(int a, int b, int c) {
+               if(a > b && a > c) {
+                   return a;
+               } else if(b > c) {
+                   return b;
+               } else {
+                   return c;
+               }
+     } 
+} 
