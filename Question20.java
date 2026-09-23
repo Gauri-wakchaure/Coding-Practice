f851d565b07245f3e30272ff2330f543dda904e2
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-23 15:34:05 +0530

    Solve Question20

---
+++ Question20.java
@@ -0,0 +1,22 @@
+public class Question20 {
+    public static void main(String[] args) {
+        int array[] = {1, 2, 3, 4, 5};
+
+        int i = 0;
+        int j = array.length-1;
+
+        while(i<j) {   
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+        i++;
+        j--;
+        }
+
+        for(int a=0; a<array.length; a++) {
+            System.out.print(array[a]);
+            System.out.print(" ");
+        }
+       
+    }
+}
