b7f389ac21b16ba67ca455084cbba946d3582d75
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-23 16:28:09 +0530

    Solve Question21

---
+++ Question21.java
@@ -0,0 +1,23 @@
+public class Question21 {
+    public static void main(String[] args) {
+        ReverseNum r1 = new ReverseNum();
+        System.out.println(r1.reverseNumber(1234));
+
+        ReverseNum r2 = new ReverseNum();
+        System.out.println(r2.reverseNumber(654321));
+    }
+}
+
+class ReverseNum {
+     int num;
+     int reverse = 0;
+    
+     public int reverseNumber(int num) {
+        while(num!=0) {
+        int last_digit = num % 10;
+        num = num / 10;
+        reverse = reverse * 10 + last_digit;
+       }    
+          return reverse;
+   }
+}
