177a646069bba8aff7dd44315def42f5ff225a5a
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-24 14:08:56 +0530

    Solve Question30

---
+++ Question30.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Question30 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(87);
+        list.add(33);
+        list.add(67);
+        list.add(52);
+        list.add(28);
+        list.add(76);
+
+        List<Integer> result = list.stream()
+            .filter(n -> n % 2 == 0)
+            .sorted()
+            .collect(Collectors.toList());
+    System.out.println(result);
+   }
+}
