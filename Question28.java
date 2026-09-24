eb7e1a15888759db9e3de9f9f4f72bab40e5e05e
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-24 13:53:02 +0530

    Solve Question28

---
+++ Question28.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+class Question28 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        list.add(87);
+        list.add(33);
+        list.add(67);
+        list.add(52);
+        list.add(28);
+
+        list.stream()
+            .filter(n -> n % 2 == 0)
+            .forEach(n -> System.out.println(n));
+    }
+}
