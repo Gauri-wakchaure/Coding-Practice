0e89ac06dc81cb0db2318ebd055f83d0317c9e10
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-24 12:41:55 +0530

    Solve Question25

---
+++ Question25.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+
+class Question25 {
+    public static void main(String[] args) {
+        HashSet<Integer> set = new HashSet<Integer>();
+        set.add(12);
+        set.add(45);
+        set.add(89);
+        set.add(14);
+        set.add(76);
+        // Duplicate Elements not allowed
+        set.add(45);
+        set.add(12);
+        System.out.println(set);
+    }
+}
