752e29101c44e22e9a98ca4fca1c9bbb339e6b09
Gauri-wakchaure <gaureewakchaure07@gmail.com>
2026-09-24 14:18:22 +0530

    Solve Question31

---
+++ Question31.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Question30 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(42);
+        list.add(8);
+        list.add(31);
+        list.add(6);
+        list.add(5);
+        list.add(12);
+        list.add(7);
+
+        List<Integer> result = list.stream()
+            .map(n -> n * n)
+            .collect(Collectors.toList());
+    System.out.println(result);
+   }
+}
